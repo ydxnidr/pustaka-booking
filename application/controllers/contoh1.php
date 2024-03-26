15f7e03b757f869efe0b6d962f82b0e5cca06bb9
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-03-26 12:02:52 +0700

    mengedit contoh1

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,7 +4,8 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>"; 
-        echo"Nama saya Indra Yudiana";
+        echo" Nama saya Indra Yudiana";
+        echo "NIM saya 15220560";
     }
 }
 
