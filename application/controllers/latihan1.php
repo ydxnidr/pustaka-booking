b75c862b91901eb9a0f634ec9acb47f7cebfce20
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-03-26 12:13:31 +0700

    edit latihan1

--- application/controllers/latihan1.php
+++ application/controllers/latihan1.php
@@ -7,6 +7,6 @@ public function index() {
 public function penjumlahan($n1, $n2) { 
     $this->load->model('Model_latihan1'); 
     $hasil = $this->Model_latihan1->jumlah($n1, $n2); 
-    echo "Hasil Penjumlahan dari". $n1 ." + ". $n2 ." = " .$hasil; 
+    echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil; 
 } 
 }
