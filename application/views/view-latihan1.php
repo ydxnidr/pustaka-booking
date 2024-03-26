8a2b7fc60ffca93d473029003c246880de0b8695
Indra Yudiana <154766195+ydxnidr@users.noreply.github.com>
2024-03-26 15:00:39 +0700

    edit view-latihan1

--- application/views/view-latihan1.php
+++ application/views/view-latihan1.php
@@ -5,6 +5,6 @@
     <h2>Halo Kawan.. Yuk kita belajar web programming..!!!</h2>
     Nilai 1 = <?= $nilai1; ?><br>
     Nilai 2 = <?= $nilai2; ?><br>
-    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
 </body> 
 </html>
